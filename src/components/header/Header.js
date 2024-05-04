21720e2294c7f9f18838955c3ed8c113e7146d1e
Nikita Lagutkin <nikitalagut03@gmail.com>
2024-05-04 13:15:12 +0300

    added logout feature

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,39 +4,56 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink, useNavigate } from "react-router-dom"; // Импортируйте useNavigate
+import { NavLink, useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react'; // Импорт useState и useEffect для работы с состоянием и жизненным циклом компонента
 
 const Header = () => {
-    const navigate = useNavigate(); // Инициализируйте useNavigate
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Состояние, отражающее статус входа пользователя
 
-    // Функция для перенаправления на страницу входа
+    useEffect(() => {
+        // Проверяем наличие токена при монтировании компонента
+        const token = localStorage.getItem('auth_token');
+        setIsLoggedIn(!!token);
+    }, []);
+
     const handleLogin = () => {
         navigate('/login');
     };
 
-    // Функция для перенаправления на страницу регистрации
     const handleRegister = () => {
         navigate('/register');
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('auth_token');
+        window.location.reload();
     };
 
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="/" style={{"color":'gold'}}>
-                    <FontAwesomeIcon icon={faVideoSlash} />Gold
+                <Navbar.Brand href="/" style={{ "color": 'gold' }}>
+                    <FontAwesomeIcon icon={faVideoSlash} /> Gold
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
-                        <Nav
-                            className="me-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
                         <NavLink className="nav-link" to="/">Главная</NavLink>
                         <NavLink className="nav-link" to="/watchlist">Список просмотра</NavLink>
                     </Nav>
-                    <Button variant="outline-info" className="me-2" onClick={handleLogin}>Вход</Button>
-                    <Button variant="outline-info" onClick={handleRegister}>Регистрация</Button>
+                    {isLoggedIn ? (
+                        <Button variant="outline-danger" onClick={handleLogout}>Выйти</Button>
+                    ) : (
+                        <>
+                            <Button variant="outline-info" className="me-2" onClick={handleLogin}>Вход</Button>
+                            <Button variant="outline-info" onClick={handleRegister}>Регистрация</Button>
+                        </>
+                    )}
                 </Navbar.Collapse>
             </Container>
         </Navbar>
